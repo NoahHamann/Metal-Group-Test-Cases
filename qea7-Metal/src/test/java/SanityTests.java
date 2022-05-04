dce69e2e6557fb65a592bbc52894f2cfc014ab0c
gssiler <33408643+gssiler@users.noreply.github.com>
2022-05-03 22:53:21 -0400

    imported Mac ChromeDriver and set path.

---
+++ qea7-Metal/src/test/java/SanityTests.java
@@ -0,0 +1,31 @@
+import static org.testng.Assert.assertFalse;
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SanityTests {
+
+	@BeforeMethod
+	public void beforeMethod() {
+
+	}
+
+	@Test
+	public void canLaunchWebdriver() {
+
+		URL driverPath = getClass().getResource("Mac/chromedriver");
+
+		System.setProperty("webdriver.chrome.driver", driverPath.getPath());
+
+
+
+		WebDriver driver = new ChromeDriver();
+
+		assertFalse(driver == null, "the webdriver should be initialized");
+	}
+
+}
