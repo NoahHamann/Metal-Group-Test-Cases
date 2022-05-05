65bb2ab0f7560f09ddeb9cc539db901f5c6bf376
gssiler <33408643+gssiler@users.noreply.github.com>
2022-05-04 23:46:19 -0400

    Complete Login Automation Test.

--- qea7-Metal/src/test/java/LoginTestGS.java
+++ qea7-Metal/src/test/java/LoginTestGS.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class AutomationSanityTestsGS {
+public class LoginTestGS {
 	private WebDriver driver;
 
 	@BeforeMethod
