5507447271789e02623d81d6092912b107ec88bc
gssiler <33408643+gssiler@users.noreply.github.com>
2022-05-05 19:59:03 -0400

    Implements test base.

--- qea7-Metal/src/test/java/LoginTestGS.java
+++ qea7-Metal/src/test/java/LoginTestGS.java
@@ -1,64 +1,32 @@
-import static org.testng.Assert.assertFalse;
-
-import java.net.URL;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTestGS {
-	WebDriver driver;
+public class LoginTestGS extends TestBase {
 	WebDriverWait wait;
-
-	@BeforeMethod
-	public void beforeMethod() {
-		URL driverPathForWindows = getClass().getResource("windows/chromedriver.exe");
-		URL driverPathPathForMac = getClass().getResource("mac/chromedriver");
-		String os = System.getProperty("os.name").toLowerCase();
-
-		if (os.contains("mac")) {
-			System.setProperty("webdriver.chrome.driver", driverPathPathForMac.getPath());
-		}
-		if (os.contains("windows")) {
-			System.setProperty("webdriver.chrome.driver", driverPathForWindows.getPath());
-		}
-		driver = new ChromeDriver();
-		// driver.navigate().to("http://invenauto.tech/index.php");
-
-		assertFalse(driver == null, "the webdriver should be initialized");
-
-	}
 
 	@Test
 	public void canLogIntoAccount() {
 
-		driver.manage().window().maximize();
-		driver.get("http://invenauto.tech/index.php?controller=authentication&back=my-account");
+		this.getDriver().manage().window().maximize();
+		this.getDriver().get("http://invenauto.tech/index.php?controller=authentication&back=my-account");
 
-		WebElement username = driver.findElement(By.id("email"));
+		WebElement username = this.getDriver().findElement(By.id("email"));
 
-		WebElement password = driver.findElement(By.id("passwd"));
+		WebElement password = this.getDriver().findElement(By.id("passwd"));
 
-		WebElement login = driver.findElement(By.id("SubmitLogin"));
+		WebElement login = this.getDriver().findElement(By.id("SubmitLogin"));
 
 		username.sendKeys("fake_email@gmail.com");
 		password.sendKeys("Abcd1234");
 		login.click();
 
 		String actualUrl = "http://invenauto.tech/index.php?controller=my-account";
-		String expectedUrl = driver.getCurrentUrl();
+		String expectedUrl = this.getDriver().getCurrentUrl();
 		Assert.assertEquals(actualUrl, expectedUrl, "Exepcted post-login and actual post-login URL's do not match.");
 
-	}
-
-	@AfterMethod
-	public void afterMethod() {
 	}
 
 }
---
+++ qea7-Metal/src/test/java/TestBase.java
@@ -0,0 +1,38 @@
+import java.net.URL;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase {
+
+	private ChromeDriver driver;
+
+	@BeforeMethod
+	public void launchDriver() {
+		URL driverPathForWindows = getClass().getResource("windows/chromedriver.exe");
+		URL driverPathPathForMac = getClass().getResource("mac/chromedriver");
+		String os = System.getProperty("os.name").toLowerCase();
+
+		if (os.contains("mac")) {
+			System.setProperty("webdriver.chrome.driver", driverPathPathForMac.getPath());
+		}
+		if (os.contains("windows")) {
+			System.setProperty("webdriver.chrome.driver", driverPathForWindows.getPath());
+		}
+		this.driver = new ChromeDriver();
+		// driver.navigate().to("http://invenauto.tech/index.php");
+
+	}
+
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+
+	public ChromeDriver getDriver() {
+		return driver;
+	}
+
+}
