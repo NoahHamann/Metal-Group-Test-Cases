9adab82b086ef42a05e2a6970bbb4364b970d1f5
gssiler <33408643+gssiler@users.noreply.github.com>
2022-05-05 20:17:41 -0400

    Renamed Class to LoginTests

--- qea7-Metal/src/test/java/LoginTests.java
+++ qea7-Metal/src/test/java/LoginTests.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTestGS extends TestBase {
+public class LoginTests extends TestBase {
 	WebDriverWait wait;
 
 	@Test
